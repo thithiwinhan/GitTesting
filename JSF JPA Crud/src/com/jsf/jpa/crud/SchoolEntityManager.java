d7fc1af9b5acfcfcf0e1c81a2dfe8308b569ac9b
Nikita <thithiwinhan.it@gmail.com>
2022-04-16 15:50:44 +0630

    fixing red error

--- JSF JPA Crud/src/com/jsf/jpa/crud/SchoolEntityManager.java
+++ JSF JPA Crud/src/com/jsf/jpa/crud/SchoolEntityManager.java
@@ -1,5 +1,6 @@
 package com.jsf.jpa.crud;
 
+import javax.faces.bean.ManagedBean;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -8,6 +9,7 @@ import javax.persistence.Table;
 
 @Entity
 @Table(name="school")
+@ManagedBean
 public class SchoolEntityManager {
 
 	@Id
