f4ff24bb046301f69bb8fddfcd81693e423a55f6
Nikita <thithiwinhan.it@gmail.com>
2022-04-28 15:30:34 +0630

    add pha an

--- JSF JPA Crud/src/com/jsf/jpa/crud/sql/copy/SQLQueries.sql
+++ JSF JPA Crud/src/com/jsf/jpa/crud/sql/copy/SQLQueries.sql
@@ -25,5 +25,7 @@ INSERT INTO school (id, name) VALUES (10, "GREENWICH ADDBY A");
 INSERT INTO school (id, name) VALUES (11, "ygn");
 INSERT INTO school (id, name) VALUES (12, "MyitKyinar");
 INSERT INTO school (id, name) VALUES (13, "KantharYar");
+INSERT INTO school (id, name) VALUES (13, "pha an");
+
 
 
