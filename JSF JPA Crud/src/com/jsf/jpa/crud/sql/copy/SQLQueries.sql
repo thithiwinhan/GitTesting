4ea5674eb500245a1bc4033380e9ffae0f8b40b8
Nikita <thithiwinhan.it@gmail.com>
2022-04-28 15:21:14 +0630

    add new shool and change label

--- JSF JPA Crud/src/com/jsf/jpa/crud/sql/copy/SQLQueries.sql
+++ JSF JPA Crud/src/com/jsf/jpa/crud/sql/copy/SQLQueries.sql
@@ -23,5 +23,7 @@ INSERT INTO school (id, name) VALUES (8, "NUS ADDBY A");
 INSERT INTO school (id, name) VALUES (9, "MIT A");
 INSERT INTO school (id, name) VALUES (10, "GREENWICH ADDBY A");
 INSERT INTO school (id, name) VALUES (11, "ygn");
-INSERT INTO school (id, name) VALUES (11, "mdy");
+INSERT INTO school (id, name) VALUES (12, "MyitKyinar");
+INSERT INTO school (id, name) VALUES (13, "KantharYar");
+
 
